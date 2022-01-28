bbe61dcb1075433e71adeed7c5e43d91a65a6361
Kirill Keller <kellertab@gmail.com>
2022-01-28 14:43:54 +0100

    Added cos method to Embedder class

--- embedder.py
+++ embedder.py
@@ -3,7 +3,11 @@ Created on 2022 Jan 28 14:09
 
 @author: keller
 """
+
+import ruclip
 import abc
+import torch
+
 
 class Embedder(abc.ABC):
     @abc.abstractmethod
@@ -18,6 +22,11 @@ class Embedder(abc.ABC):
     def encode_imgs(self, imgs):
         pass
 
-    @abc.abstractmethod
-    def cos(a, b):
-       pass
+    def cos(self, text_emb: torch.Tensor, img_emb: torch.Tensor) -> torch.Tensor:
+        """
+        Returns cos similarity between text_emb and img_emb
+        :param text_emb: tensor, shape 1x512
+        :param img_emb: tensor, shape 1x512
+        :return: cos similarity
+        """
+        return torch.dot(text_emb, img_emb)
