35f7a7b9c94b58b603d8842baa902877d6df91ca
Dronov Vasiliy <88777856+luckyseadog@users.noreply.github.com>
2022-01-30 23:26:23 +0800

    Add files via upload

---
+++ faiss.py
@@ -0,0 +1,35 @@
+import pickle
+import faiss
+
+class FAISS_index:
+
+  def __init__(self, dim, param, nprobe=5):
+    self.index = faiss.index_factory(dim, param)
+    self.dim = dim
+    self.index.nprobe = nprobe
+  
+  def add(self, vs):
+    self.index.add(vs)
+
+  def train(self, train_vectors):
+    self.index.train(train_vectors) 
+
+  def find(self, query, topn):
+    D, I = self.index.search(query, topn) 
+    return D, I
+
+  def save(self,index):
+    faiss.write_index(self.index, "flat.index")
+    '''
+    f = open(file, 'wb')
+    pickle.dump(self.index, file)
+    f.close()
+    '''
+
+  def load(self, file):
+    self.index = faiss.read_index("file")
+    '''
+    f = open(file, 'rb')
+    self.index = pickle.load(f)
+    f.close()
+    '''
