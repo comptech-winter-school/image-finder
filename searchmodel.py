88f85ed1e6a2b106cb7a0924b0f5c882a1591677
Kirill Keller <kellertab@gmail.com>
2022-01-30 20:07:30 +0100

    Backbone of SearchModel

--- searchmodel.py
+++ searchmodel.py
@@ -6,5 +6,25 @@ Created on 2022 Jan 30 10:14
 
 
 class SearchModel():
-    def __init__(self):
+    def __init__(self, embedder, indexer):
+        self.embedder = embedder
+        self.indexer = indexer
+
+    def load_imgs(self, path):
         pass
+
+    def get_embeddings(self):
+        pass
+
+    def load_embeddings(self):
+        pass
+
+    def get_k_imgs(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
