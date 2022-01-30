dedc79a5fcfac16d6e12b61d8886f30892d9cb4a
Dmitry <44023937+EnterSub@users.noreply.github.com>
2022-01-30 18:22:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ def function_images():
     input_format = {f'({i+1}).jpg': j for i, j in zip(range(values), cosine_distance)}
     input_format = dict(sorted(input_format.items(), key=lambda item: item[1], reverse=True))
 
-    for i in input_format.items():
-        image = Image.open(i[0])
-        st.image(image, caption=f'{i[0]}, {i[1]}')
+    col1, col2 = st.columns([1, 1])
 
     for i, j in zip(input_format.keys(), input_format.values()):
         data.append([i, j])
@@ -24,7 +22,14 @@ def function_images():
         df = df.append(data)
         df = df.rename(columns={0: "Image", 1: "Cosine distance"})
 
-    st.dataframe(df.style.text_gradient(axis=0, cmap='Spectral'))
+    for _, i in enumerate(input_format.items(), 1):
+        with col1:
+            image = Image.open(i[0])
+            st.image(image, caption=f'{i[0]}, {i[1]}')
+        with col2:
+            if _ == 1:
+                st.write("Output cosine distance")
+                st.dataframe(df.style.text_gradient(axis=0, cmap='Spectral'))
 
 
 st.image(Image.open('logo.png'))
@@ -46,17 +51,16 @@ else:
 #Image
     file = st.file_uploader("Choose image", type=['jpeg', 'jpg', 'png'], accept_multiple_files=False)
 values = st.slider('Select a range of sample images', 1, 10, 5)
-st.write('Values:', values)
 
 #Processing
 if st.button('Process'):
     if option == 'Text query' and text:
-        st.write(f'Output images and cosine distance for text query: {text}')
+        st.write(f'Output images for text query: {text}')
         function_images()
     elif option == 'Image' and file is not None:
         image = Image.open(file.name)
         st.image(image, caption=file.name)
-        st.write(f'Output images and cosine distance for current input image: {file.name}')
+        st.write(f'Output images for current input image: {file.name}')
         function_images()
     else:
         st.write("Error")
