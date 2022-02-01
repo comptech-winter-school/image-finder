36251cfd3b1d67b4131be95aa16031ea0a807b7e
Kirill Keller <kellertab@gmail.com>
2022-02-01 13:22:26 +0100

    Finished DummyIndexer and added tests

--- dummyindexer.py
+++ dummyindexer.py
@@ -9,25 +9,54 @@ import numpy as np
 
 class DummyIndexer():
     def __init__(self):
+        """
+        Creates an empty index object
+        """
         self.index = None
 
     def add(self, embs: np.ndarray):
+        """
+        Adds new embeddings embs in empty or existing index
+        :param embs:
+        :return:
+        """
         if self.index is None:
             self.index = embs
         else:
-            np.append(self.index, embs, axis=0)
+            self.index = np.append(self.index, embs, axis=0)
 
     def train(self):
+        """
+        Not sure if this one is necessary here, left for compatibility with abstract class Indexer
+        :return:
+        """
         pass
 
-    def find(self, query: np.ndarray, topn: int):
-        similarities = (self.index @ query.T())
+    def find(self, query: np.ndarray, topn: int) -> (np.ndarray, np.ndarray):
+        """
+        Returns topn entries closest to the query vector
+        :param query:
+        :param topn:
+        :return:
+        """
+        similarities = (self.index @ query.squeeze())
         best_photo_idx = (-similarities).argsort()
         D, I = similarities[best_photo_idx[:topn]], best_photo_idx[:topn]
         return D, I
 
-    def save(self, file):
-        np.save(self.index)
+    def save(self, file: str):
+        """
+        Saves data to npy file
+        :param file:
+        :return:
+        """
+        np.save(file, self.index)
 
-    def load(self, file):
+    def load(self, file: str):
+        """
+        Loads data from npy file
+        :param file:
+        :return:
+        """
         self.index = np.load(file)
+
