a6abce0c1742ba08d69c01d80e742decca099971
Dronov Vasiliy <88777856+luckyseadog@users.noreply.github.com>
2022-02-01 10:43:33 +0800

    Update faiss.py

--- faiss.py
+++ faiss.py
@@ -1,16 +1,20 @@
 import pickle
 import faiss
+import abc
 
-class FAISS_index:
-
+class FaissIndexer:
+  
+  @abc.abstractmethod
   def __init__(self, dim, param, nprobe=5):
     self.index = faiss.index_factory(dim, param)
     self.dim = dim
     self.index.nprobe = nprobe
   
+  
   def add(self, vs):
     self.index.add(vs)
-
+  
+  @abc.abstractmethod
   def train(self, train_vectors):
     self.index.train(train_vectors) 
 
